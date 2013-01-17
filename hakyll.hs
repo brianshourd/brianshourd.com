ff79eb83d847bf7cee321c684c965f2b41246383
Brian Shourd <brian.shourd@gmail.com>
2013-01-16 22:37:29 -0500

    At least it compiles...

--- hakyll.hs
+++ hakyll.hs
@@ -4,17 +4,24 @@ module Main where
 import Prelude hiding (id)
 import Control.Category (id)
 import Control.Arrow ((>>>), (***), (^>>), arr, second)
-import Data.Monoid (mempty, mconcat)
+import Data.Monoid (mempty, mconcat, mappend)
 import Data.Map (fromList)
 import Data.Char (ord)
 import Text.Pandoc (WriterOptions (..), HTMLMathMethod (MathJax))
 
 import Hakyll
 
-config :: HakyllConfiguration
-config = defaultHakyllConfiguration {
+config :: Configuration
+config = defaultConfiguration {
     deployCommand = "rsync -avcz _site/ briansho@brianshourd.com:public_html/"
 }
+
+-- context :: Context
+context = mconcat
+    [constField "siteTitle" "Blog",
+    constField "mathjax" "",
+    constField "colorize" "",
+    defaultContext]
 
 main :: IO ()
 main = hakyllWith config $ do
@@ -24,68 +31,73 @@ main = hakyllWith config $ do
         compile copyFileCompiler
 
     -- Compress CSS
---    match "css/*" $ do
---        route   idRoute
---        compile compressCssCompiler
     match "css/style.scss" $ do
         route   $ setExtension ".css"
-        compile $ getResourceString 
-            >>> sassifyString
+        compile $ sassifyString
 
     -- Render posts
     match "posts/*" $ do
         route   $ setExtension ".html"
-        compile $ pageCompilerWith defaultHakyllParserState pandocOptions
+        compile $ do
+            pandocCompilerWith defaultHakyllParserState pandocOptions
             -- To load MathJax javascript or not
-            >>> arr checkMathOption
+--            >>= arr checkMathOption
             -- for RSS feed
-            >>> arr (copyBodyToField "description")
-            >>> colorizePage
-            >>> applyTemplateCompiler "templates/post.html"
-            >>> arr (setField "siteTitle" "Blog")
-            >>> applyTemplateCompiler "templates/default.html"
-            >>> relativizeUrlsCompiler
+--            >>= arr (copyBodyToField "description")
+--            >>= colorizePage
+            >>= loadAndApplyTemplate "templates/post.html" context
+--            >>= arr (setField "siteTitle" "Blog")
+            >>= loadAndApplyTemplate "templates/default.html" context
+            >>= relativizeUrls
 
     -- Render posts list
-    match  "posts.html" $ route idRoute
-    create "posts.html" $ constA mempty
+--    match  "posts.html" $ route idRoute
+    create ["posts.html"] $ do
+        route idRoute
+        compile $ do
         -- To load MathJax javascript or not
-        >>> arr checkMathOption
-        >>> colorizePage
-        >>> arr (setField "siteTitle" "All Posts")
-        >>> requireAllA "posts/*" addPostList
-        >>> applyTemplateCompiler "templates/posts.html"
-        >>> applyTemplateCompiler "templates/default.html"
-        >>> relativizeUrlsCompiler
+--        >>= arr checkMathOption
+--        >>= colorizePage
+--        >>= arr (setField "siteTitle" "All Posts")
+--        >>= requireAllA "posts/*" addPostList
+            makeItem ""
+            >>= loadAndApplyTemplate "templates/posts.html" (context `mappend` constField "posts" "")
+            >>= loadAndApplyTemplate "templates/default.html" context
+            >>= relativizeUrls
 
     -- Index
-    match  "index.html" $ route idRoute
-    create "index.html" $ constA mempty
+--    match  "index.html" $ route idRoute
+    create ["index.html"] $ do
+        route idRoute
+        compile $ do
         -- To load MathJax javascript or not
-        >>> arr checkMathOption
-        >>> colorizePage
-        >>> arr (setField "siteTitle" "Home")
-        >>> requireAllA "posts/*" (id *** arr (take 3 . reverse . chronological) >>> addPostList)
-        >>> applyTemplateCompiler "templates/index.html"
-        >>> applyTemplateCompiler "templates/default.html"
-        >>> relativizeUrlsCompiler
+--        >>= arr checkMathOption
+--        >>= colorizePage
+--        >>= arr (setField "siteTitle" "Home")
+--        >>= requireAllA "posts/*" (id *** arr (take 3 . reverse . chronological) >>= addPostList)
+            makeItem ""
+            >>= loadAndApplyTemplate "templates/index.html" (context `mappend` constField "posts" "")
+            >>= loadAndApplyTemplate "templates/default.html" context
+            >>= relativizeUrls
 
     -- About
     match "about.markdown" $ do
         route   $ setExtension ".html"
-        compile $ pageCompiler
+        compile $ pandocCompiler
             -- To load MathJax javascript or not
-            >>> arr checkMathOption
-            >>> colorizePage
-            >>> arr (setField "siteTitle" "About")
-            >>> applyTemplateCompiler "templates/default.html"
-            >>> relativizeUrlsCompiler
+--            >>= arr checkMathOption
+--            >>= colorizePage
+--            >>= arr (setField "siteTitle" "About")
+            >>= loadAndApplyTemplate "templates/default.html" context
+            >>= relativizeUrls
             
     -- Render RSS feed
+    {-
     match  "rss.xml" $ route idRoute
     create "rss.xml" $
         requireAll_ "posts/*" 
-        >>> renderRss feedConfiguration
+        >>= renderRss feedConfiguration
+    -}
 
     -- Read templates
     match "templates/*" $ compile templateCompiler
@@ -107,12 +119,14 @@ pandocOptions = defaultHakyllWriterOptions
 -- | Auxiliary compiler: generate a post list from a list of given posts, and
 -- add it to the current page under @$posts@
 --
+{-
 addPostList :: Compiler (Page String, [Page String]) (Page String)
 addPostList = setFieldA "posts" $
     arr (reverse . chronological)
-        >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
-        >>> arr mconcat
-        >>> arr pageBody
+        >>= require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
+        >>= arr mconcat
+        >>= arr pageBody
+-}
 
 -- Take in a string (url is used), and perform a
 -- simple hash to get a valid hsl color for sass
@@ -121,22 +135,26 @@ hashColor s = "hsl(" ++ hash ++ ", 39, 60)" where
     hash = show . (flip mod) 360 . sum . map ord $ s
 
 -- Run sass, then compress
-sassifyString :: Compiler String String
-sassifyString = unixFilter "sass" ["-s", "--scss", "--trace", "--load-path", "css"]
-    >>> arr compressCss
+-- sassifyString :: Compiler String String
+sassifyString = getResourceString
+    >>= withItemBody (unixFilter "sass" ["-s", "--scss", "--trace", "--load-path", "css"])
+    >>= return . fmap compressCss
 
 -- Fill in the $css$ option with colors!
+{-
 colorizePage :: Compiler (Page String) (Page String)
 colorizePage = requireA "templates/colorize.scss" $
     arr (\(p, t) -> (p,pageFromTemplate t $ getField "url" p))
-        >>> second (pageBody ^>> sassifyString)
-        >>> arr (uncurry . flip $ setField "colorize")
+        >>= second (pageBody ^>> sassifyString)
+        >>= arr (uncurry . flip $ setField "colorize")
         where
             pageFromTemplate t s = applyTemplate t $ fromMap . fromList $ [("color", hashColor s)]
+-}
 
+{-
 checkMathOption :: Page String -> Page String
 checkMathOption page =
     case getFieldMaybe "math" page of
         Nothing -> setField "mathjax" "" page
         Just _  -> setField "mathjax" "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\" />" page
-
+-}
